9e4158420c4f02dbf85eb4de20ff842aba351614
Carl Jones <jonesc162@cardiff.ac.uk>
2021-02-05 13:39:18 +0000

    Initial Commit

---
+++ src/main/java/com/cm6123/wormhole/app/Application.java
@@ -0,0 +1,34 @@
+package com.cm6123.wormhole.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public final class Application {
+  /**
+   * Create a logger for the class.
+   */
+  private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+
+  private Application() {
+  }
+
+  /**
+   * main entry point.  If args provided, result is displayed and program exists. Otherwise, user is prompted for
+   * input.
+   *
+   * @param args command line args.
+   */
+  public static void main(final String[] args) {
+
+
+    logger.info("Application Started with args:{}", String.join(",", args));
+
+    System.out.println("Hello World.  Welcome to Wormhole.");
+
+    logger.info("Application closing");
+  }
+
+
+}
