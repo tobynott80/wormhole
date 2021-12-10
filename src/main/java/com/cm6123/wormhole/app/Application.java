10a7dc37e3b8c7c974ac2364860e4eaa0acc65cf
Carl Jones <jonesc162@cardiff.ac.uk>
2021-12-10 12:22:52 +0000

    Update artifacts and use comsc runner.

--- src/main/java/com/cm6123/wormhole/app/Application.java
+++ src/main/java/com/cm6123/wormhole/app/Application.java
@@ -5,30 +5,30 @@ import org.slf4j.LoggerFactory;
 
 
 public final class Application {
-  /**
-   * Create a logger for the class.
-   */
-  private static Logger logger = LoggerFactory.getLogger(Application.class);
+    /**
+     * Create a logger for the class.
+     */
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
 
 
-  private Application() {
-  }
+    private Application() {
+    }
 
-  /**
-   * main entry point.  If args provided, result is displayed and program exists. Otherwise, user is prompted for
-   * input.
-   *
-   * @param args command line args.
-   */
-  public static void main(final String[] args) {
+    /**
+     * main entry point.  If args provided, result is displayed and program exists. Otherwise, user is prompted for
+     * input.
+     *
+     * @param args command line args.
+     */
+    public static void main(final String[] args) {
 
 
-    logger.info("Application Started with args:{}", String.join(",", args));
+        logger.info("Application Started with args:{}", String.join(",", args));
 
-    System.out.println("Hello World.  Welcome to Wormhole.");
+        System.out.println("Hello World.  Welcome to Wormhole.");
 
-    logger.info("Application closing");
-  }
+        logger.info("Application closing");
+    }
 
 
 }
