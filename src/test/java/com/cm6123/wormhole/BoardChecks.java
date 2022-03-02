f1aeeeac1f83babe0a62227e3d2444cc76ee5e24
Toby Nott <NottTJ@cardiff.ac.uk>
2022-03-02 23:35:31 +0000

    added first boardcheck test to check if a board can be created

--- src/test/java/com/cm6123/wormhole/BoardChecks.java
+++ src/test/java/com/cm6123/wormhole/BoardChecks.java
@@ -1,4 +1,17 @@
 package com.cm6123.wormhole;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import com.cm6123.wormhole.game.GameBoard;
+
+
+
 public class BoardChecks {
+
+    @Test
+    public void shouldBeAbleToCreateBoardOf5 (){
+        GameBoard newBoard = new GameBoard(5);
+        assertEquals(newBoard.getBoardSize(), 25);
+    }
 }
