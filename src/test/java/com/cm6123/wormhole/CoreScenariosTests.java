ca6b9eba815fd96d7dceaa94e6cc1a815ae298b5
Toby Nott <NottTJ@cardiff.ac.uk>
2022-03-02 23:55:58 +0000

    updated gitignore with reference files

--- src/test/java/com/cm6123/wormhole/CoreScenariosTests.java
+++ src/test/java/com/cm6123/wormhole/CoreScenariosTests.java
@@ -1,4 +1,24 @@
 package com.cm6123.wormhole;
 
+import com.cm6123.wormhole.game.GameBoard;
+import org.junit.jupiter.api.Test;
+
 public class CoreScenariosTests {
+
+    @Test
+    private void shouldBeAbleToCompleteTest1(){
+        /**
+         * Given:
+         *   A game is being played on a boardof size 4 with no wormholes and all players are on square 1 and it is player 1’s turn
+         *
+         * When:
+         *  A 3 and 4 are rolled
+         * Then:
+         *  Player 1 ends on square 7 and it is player 2’s turn
+         */
+        GameBoard gb = new GameBoard(4); //initialise new gameboard of width
+
+
+
+    }
 }
