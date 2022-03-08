64ca7d4a2cc7a37da3f718b7f353567801b27c03
Toby Nott <NottTJ@cardiff.ac.uk>
2022-03-08 21:01:45 +0000

    updated core scenarios

--- src/test/java/com/cm6123/wormhole/CoreScenariosTests.java
+++ src/test/java/com/cm6123/wormhole/CoreScenariosTests.java
@@ -5,23 +5,26 @@ import com.cm6123.wormhole.player.Player;
 import com.cm6123.wormhole.player.PlayerController;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CoreScenariosTests {
 
     @Test
-    private void shouldBeAbleToCompleteTest1(){
-        /**
-         * Given:
-         *   A board is being played on a boardof size 4 with no wormholes and all players are on square 1 and it is player 1’s turn
-         *
-         * When:
-         *  A 3 and 4 are rolled
-         * Then:
-         *  Player 1 ends on square 7 and it is player 2’s turn
-         */
-        GameBoard gb = new GameBoard(4); //initialise new gameboard of width
-        PlayerController controller = new PlayerController(2);
+    public void shouldBeAbleToCompleteTest1() {
+         //
+         // Given:
+         //   A board is being played on a boardof size 4 with no wormholes
+         //   and all players are on square 1 and it is player 1’s turn
+         //
+         // When:
+         //  A 3 and 4 are rolled
+         // Then:
+         //  Player 1 ends on square 7 and it is player 2’s turn
 
-        Player player1 = new Player(1);
+        GameBoard gb = new GameBoard(4); //initialise new gameboard of width
+        PlayerController controller = new PlayerController(2); //create new 2 player controller
+        controller.initialisePlayers(1); //initialise players starting at 1
+        assertEquals(controller.playerList.get(0).getPosition(),1); //check that player 1 has been initialised and is at the correct position
 
     }
 }
