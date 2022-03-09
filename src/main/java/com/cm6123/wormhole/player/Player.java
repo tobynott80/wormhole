6eb5f72257adbceb6615d19b4cd573960a833752
Toby Nott <NottTJ@cardiff.ac.uk>
2022-03-09 16:43:05 +0000

    fixed checkstyle errors in player class

--- src/main/java/com/cm6123/wormhole/player/Player.java
+++ src/main/java/com/cm6123/wormhole/player/Player.java
@@ -1,23 +1,33 @@
 package com.cm6123.wormhole.player;
 
 /**
- * Class to manage an individual player and their actions
+ * Class to manage an individual player and their actions.
  */
-public class Player {
+public final class Player {
 
+    /**
+     * Integer variable with the square position of the current location of the player.
+     */
     private int position;
 
-    public Player(int startingPosition) {
+    /**
+     * Constructor for mostly testing purposes to define the players initial starting position.
+     * @param startingPosition Location for where player starts. Default is 1
+     */
+    public Player(final int startingPosition) {
         position = startingPosition;
     }
 
     /**
-     * Constructor creating new player at position 1
+     * Constructor creating new player at position 1.
      */
     public Player() {
         this.position = 1;
     }
 
+    /**
+     * @return Returns the current position of the player.
+     */
     public int getPosition() {
         return position;
     }
@@ -25,7 +35,7 @@ public class Player {
     /**
      * @param squareDistance How many squares should player be moved
      */
-    public void movePlayer(int squareDistance) {
+    public void movePlayer(final int squareDistance) {
         int targetSquare = this.getPosition() + squareDistance;
         System.out.println(targetSquare);
         position = targetSquare;
